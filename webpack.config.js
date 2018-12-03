cd709638b3d3acb368bcbcd667d945294e8fdf44
Elena Anischenkova <eanischenkova@gmail.com>
2018-12-03 23:35:41 +0300

    webpack init

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HTMLPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{ loader: 'style-loader' },
+					{ loader: 'css-loader' },
+					{ loader: 'sass-loader' },
+				],
+			},
+		],
+	},
+	plugins: [
+		new HTMLPlugin({
+			template: 'index.html',
+		}),
+	],
+}
